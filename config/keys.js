7e54e8356693a367ee8edd2d0bb8366733b366e5
Maaz-Mehtab <maazmehtabuddin95@gmail.com>
2018-08-20 20:27:16 +0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports ={
-    mongoURI:'mongodb://Maaz:03243424011Mohsin@ds011268.mlab.com:11268/mern-shooping'
+    mongoURI:'mongodb://UserName:Password@ds011268.mlab.com:11268/mern-shooping'
 };
